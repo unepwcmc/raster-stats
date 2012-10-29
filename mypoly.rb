4b7bd28768b78091ea656af4bf42539fdb7ce29c
Miguel Torres <miguel.torres@unep-wcmc.org>
2012-10-29 17:27:26 +0000

    Saves results in a JSON file

--- mypoly.rb
+++ mypoly.rb
@@ -1,28 +1,51 @@
 require 'rubygems'
 require 'sinatra'
 require "json"
+require "csv"
 
 STARSPAN = 'starspan'
 LOW_RES_PATH = 'raster/low_resolution/'
-RESULTS_PATH = 'results/'
+MEDIUM_RES_PATH = 'raster/medium_resolution/'
+HIGH_RES_PATH = 'raster/high_resolution/'
+RESULTS_PATH= 'results/raster_'
 
 get '/' do
   'hello world'
 end
 
 get '/stats/:polygon' do
-  polygon_file = "user_polygon.geojson"
+	TIME = Time.now.getutc.to_i
+  content_type :json
+  polygon_file = "tmp/user_polygon_#{TIME}.geojson"
   File.open(polygon_file, 'w'){|f| f.write(params[:polygon])}
   polygon = JSON.parse(params[:polygon])
-  #json.to_s + "<br />" + json["features"].to_s #["properties"]["area"].to_i
   area= polygon["features"][0]["properties"]["AREA"]
-  "imprimir " + area.to_s
   if area > 9_000_000_000_000
-  raster_path = LOW_RES_PATH + 'carbon.tif'
+    raster_path = LOW_RES_PATH + 'carbon.tif'
+  elsif area > 600_000_000_000
+    raster_path = MEDIUM_RES_PATH + 'carbon.tif'
+  else
+    raster_path = HIGH_RES_PATH + 'carbon.tif'
   end
   stats = "avg sum"
-  call = "#{STARSPAN} --vector '#{polygon_file}' --raster #{raster_path} --stats #{stats} --out-prefix raster --out-type table --summary-suffix _stats.csv"
+  call = "#{STARSPAN} --vector '#{polygon_file}' --raster #{raster_path} --stats #{stats} --out-prefix #{RESULTS_PATH} --out-type table --summary-suffix #{TIME}.csv"
   puts call
-  system call
-  
+  system(call)
+	if File.file?("#{RESULTS_PATH}#{TIME}.csv")
+		puts "File generated successfuly"
+    csv_table = CSV.read("#{RESULTS_PATH}#{TIME}.csv", {headers: true})
+    list = []
+		csv_table.each do |row|
+			entry = {}
+			csv_table.headers.each do |header|
+				entry[header] = row[header]
+			end
+			list << entry
+		end
+		result = JSON.pretty_generate(list)
+		puts result
+		result
+	else 
+    {}
+	end
 end
