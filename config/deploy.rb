116431e49b53a9e1a87bbc259d93e1726bb3a837
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-11-21 10:02:57 +0000

    Deploy app with SQLite3 database

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,33 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
+
+# SQLite3 configuration
+set(:shared_database_path) {"#{shared_path}/databases"}
+
+namespace :sqlite3 do
+  desc "Generate a database configuration file"
+  task :build_configuration, :roles => :db do
+    db_options = {
+      "adapter"  => "sqlite3",
+      "database" => "#{shared_database_path}/production.sqlite3"
+    }
+    config_options = {"production" => db_options}.to_yaml
+    put config_options, "#{shared_config_path}/sqlite_config.yml"
+  end
+ 
+  desc "Links the configuration file"
+  task :link_configuration_file, :roles => :db do
+    run "ln -nsf #{shared_config_path}/sqlite_config.yml #{release_path}/config/database.yml"
+  end
+ 
+  desc "Make a shared database folder"
+  task :make_shared_folder, :roles => :db do
+    run "mkdir -p #{shared_database_path}"
+  end
+end
+
+after "deploy:setup", "sqlite3:make_shared_folder"
+after "deploy:setup", "sqlite3:build_configuration"
+ 
+before "deploy:migrate", "sqlite3:link_configuration_file"
