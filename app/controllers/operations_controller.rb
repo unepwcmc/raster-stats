fbcc499955eacb814605ba16540a92eed9dc23ca
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-11-26 14:34:17 +0000

    Clean some controllers

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,11 +1,9 @@
 class OperationsController < ApplicationController
-  # GET /operations.json
   def index
     @operations = Operation.all
     render json: @operations
   end
 
-  # GET /rasters/:raster_id/operations/:id
   def show
     raster = Raster.find(params[:raster_id])
     starspan = Starspan.new(raster, params[:id], params[:polygon])
--- app/controllers/rasters_controller.rb
+++ app/controllers/rasters_controller.rb
@@ -1,85 +1,49 @@
 class RastersController < ApplicationController
+  respond_to :html, :json
   before_filter :authenticate_user!, except: [:index, :show]
 
-  # GET /rasters
-  # GET /rasters.json
   def index
     @rasters = Raster.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json # index.json.rabl
-    end
+    respond_with(@rasters)
   end
 
-  # GET /rasters/1
-  # GET /rasters/1.json
   def show
     @raster = Raster.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json # show.json.rabl
-    end
+    respond_with(@raster)
   end
 
-  # GET /rasters/new
-  # GET /rasters/new.json
   def new
     @raster = Raster.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @raster }
-    end
+    respond_with(@raster)
   end
 
-  # GET /rasters/1/edit
   def edit
     @raster = Raster.find(params[:id])
+    respond_with(@raster)
   end
 
-  # POST /rasters
-  # POST /rasters.json
   def create
     @raster = Raster.new(params[:raster])
 
-    respond_to do |format|
-      if @raster.save
-        format.html { redirect_to @raster, notice: 'Raster was successfully created.' }
-        format.json { render json: @raster, status: :created, location: @raster }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @raster.errors, status: :unprocessable_entity }
-      end
+    if @raster.save
+      flash[:notice] = 'Raster was successfully created.'
     end
+    respond_with(@raster)
   end
 
-  # PUT /rasters/1
-  # PUT /rasters/1.json
   def update
     @raster = Raster.find(params[:id])
 
-    respond_to do |format|
-      if @raster.update_attributes(params[:raster])
-        format.html { redirect_to @raster, notice: 'Raster was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @raster.errors, status: :unprocessable_entity }
-      end
+    if @raster.update_attributes(params[:raster])
+      flash[:notice] = 'Raster was successfully updated.'
     end
+    respond_with(@raster)
   end
 
-  # DELETE /rasters/1
-  # DELETE /rasters/1.json
   def destroy
     @raster = Raster.find(params[:id])
     @raster.destroy
-
-    respond_to do |format|
-      format.html { redirect_to rasters_url }
-      format.json { head :no_content }
-    end
+    flash[:notice] = 'Raster was successfully destroyed.'
+    respond_with(@product)
   end
 end
