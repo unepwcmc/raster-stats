ba6488da1657df28197411373194fbb7a778a291
Decio Battaglia <decio.battaglia@gmail.com>
2013-10-21 16:35:08 +0100

    change in controller to handle iso code

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -6,7 +6,11 @@ class OperationsController < ApplicationController
 
   def show
     raster = Raster.find(params[:raster_id])
-    starspan = Starspan.new(raster, params[:id], params[:polygon])
+    if params[:iso2]
+      polygon = Countries.find_by :iso_a2 params[:iso2]
+    else
+      starspan = Starspan.new(raster, params[:id], polygon)
+    end
     render json: starspan.result
   end
 end
