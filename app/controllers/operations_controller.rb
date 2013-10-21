ed33080b9634494fc1b8901271ae01063c788c8c
Decio Battaglia <decio.battaglia@gmail.com>
2013-10-21 17:32:57 +0100

    change to raster stats api

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -7,9 +7,9 @@ class OperationsController < ApplicationController
   def show
     raster = Raster.find(params[:raster_id])
     if params[:iso2]
-      polygon = Countries.select(:geometry).where(:iso_a2 => params[:iso2]).first
+      polygon = Countries.where(:iso_a2 => params[:iso2]).first.geometry
       debugger
-      starspan = Starspan.new(raster, params[:id], polygon.geometry)
+      starspan = Starspan.new(raster, params[:id], polygon)
     else
       starspan = Starspan.new(raster, params[:id], params[:polygon])
     end
