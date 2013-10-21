4f9290256db46b1959f3f4a69791f61805dfb6aa
Decio Battaglia <decio.battaglia@gmail.com>
2013-10-21 16:27:47 +0100

    countries loading data script fixes

--- db/load_countries.rb
+++ db/load_countries.rb
@@ -2,13 +2,14 @@
 require 'rubygems'
 require 'json'
 require 'pp'
+require '../config/environment.rb'
 
-json = File.read('/home/deciob/dev/raster-stats/db/data/world_50m.geojson')
+json = File.read('./data/world_50m.geojson')
 obj = JSON.parse(json)
 
 obj['features'].each do |item| #{|item| pp item['properties']['iso_a2']}
 
-  pp item['properties']['iso_a2']
+  #pp item['properties']['iso_a2']
 
   data = {}
   data["iso_a2"] = item['properties']['iso_a2']
