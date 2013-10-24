ba8b5ca60068b397e20f0850a74d92bd054f179b
migtorres <miguel.torres@unep-wcmc.org>
2013-10-24 10:57:49 +0100

    added area to countries model - committing final load countries script

--- db/load_countries.rb
+++ db/load_countries.rb
@@ -4,22 +4,31 @@ require 'json'
 require 'pp'
 require '../config/environment.rb'
 
+
+def load_features (features)
+  features.each do |item|
+    pp item['properties']['iso_a2']
+    data = {}
+    data["iso_a2"] = item['properties']['iso_a2']
+    data["area"] = item['properties']['AREA']
+    data["geometry"] = JSON.generate(item['geometry'])
+    d = Countries.new(data)
+    d.save
+  end
+end
+
 json = File.read('./data/world_50m.geojson')
 obj = JSON.parse(json)
-
+features = obj['features']
 Countries.destroy_all
+load_features features
 
-obj['features'].each do |item| #{|item| pp item['properties']['iso_a2']}
+json = File.read('./data/world_50m_ru.geojson')
+obj = JSON.parse(json)
+features = obj['features']
+Countries.where(:iso_a2 == "RU").first.destroy
+load_features features
 
-  #pp item['properties']['iso_a2']
-  #pp item['geometry']
-  #pp JSON.load(item['geometry'])
 
-  data = {}
-  data["iso_a2"] = item['properties']['iso_a2']
-  data["geometry"] = JSON.generate(item['geometry'])
-  
-  d = Countries.new(data)
-  d.save
 
-end
+
